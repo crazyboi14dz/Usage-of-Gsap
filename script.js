a95e9190b1033851e914174b0f926591e24bb02d
crazyboi14dz <dilipkumarsahoo2003@gmail.com>
2024-08-30 23:08:50 +0530

    finshy

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ document.addEventListener("mousemove", function (dets) {
   crsrBlur.style.top = dets.y - 250 + "px";
 });
 
+var navItem = document.querySelectorAll("#nav p");
+
+navItem.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid white";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   duration: 0.5,
@@ -35,3 +50,73 @@ gsap.to("#main", {
     scrub: 2,
   },
 });
+
+gsap.from("#about-us img,#about-us-in", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  /*In stagger one by one will come while scrolling
+  0.4s delay of every element like about-us-img, about-us-in*/
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 55%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".card", {
+  scale: 0.8,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers: true,
+    start: "top 70%",
+    end: "top 55%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#quote1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#quote1",
+    scroller: "body",
+    // markers: true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#quote2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#quote1",
+    scroller: "body",
+    // markers: true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#page4>h4",{
+  y:50,
+  scrollTrigger:{
+    trigger: "#page4>h4",
+    scroller: "body",
+    // markers: true,
+    start: "top 80%",
+    end: "top 70%",
+    scrub: 2,
+  }
+})
